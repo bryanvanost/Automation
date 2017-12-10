1e44daa8927ca76bd4b33bc8ff361ed5a583a1ab
Bryan Van Ost <bryan.vanost@gmail.com>
2017-12-10 11:02:20 -0500

    a
    a

---
+++ address.py
@@ -0,0 +1,9 @@
+PLM01Adr='x0fx43x69','2412S'
+#PLM='x43x6ExFE'
+#TriLamp= insteonDevice(Office Lamp,'x0exa7xa6','2456D3')
+#Office=  insteonDevice(Office,'x09x97x01','2486D')
+#BedRm=   insteonDevice(Bed Room,'x13x99xA2','2477D')
+#LivingRm=insteonDevice(Living Room,'x1DxE3x5B','2477D')
+#DiningRm=insteonDevice(Dining Room,'x1DxDEx9A','2477D')
+#CouchLamp=insteonDevice(Couch Lamp,'x13xB3x25','2475D')
+#Other=insteonDevice(OTHER,'x13xB1x00','2475D')
