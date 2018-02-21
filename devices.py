33201a27ff5e977f17e7aced377ef251a9794428
Bryan Van Ost <bryan.vanost@gmail.com>
2018-02-21 08:23:39 -0500

    additional devices

--- devices.py
+++ devices.py
@@ -1,11 +1,15 @@
 plm=b'\x43\x6E\xFE'#2413S
 
 #5 Working
-hall=b'\x4a\x9e\x61'
-kitchen= b'\x4A\x98\x0D'
-kitchenPen= b'\x1D\xDB\xCC'
+hall=b'\x4a\x9e\x61' #2334-232
+hall2=b'\x1D\xDE\x9A' #2477D
+#installed next to 2475D2
+kitchen= b'\x4A\x98\x0D' #2334-232
+kitchenPen= b'\x1D\xDB\xCC' #2477D
+
 lamp1= b'\x0E\xA7\xA6'
 lamp2= b'\x0E\x9A\x17'
+
 wall=b'\x1D\xE3\x5B'
 upstairsBedRm=b'\x1D\xDE\x9A'
 livingRm=b'\x49\xB4\xB4'
@@ -13,3 +17,6 @@ remote1=b'\x44\x8C\x6B'
 masterBath=b'\x49\xB4\xCC'
 officeLav=b'\x49\x98\x98'
 closet1=b'\x13\x99\xA2'
+other=b'\x49\xB4\xB4' #2477D
+other2=b'\x4A\x9C\x24' #2334-232
+other3=b'\x4A\x9B\x15' #2334-232
